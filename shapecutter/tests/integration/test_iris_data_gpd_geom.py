e1a55eaa5c8c6094318130894ce2c7cdd17fd94c
DPeterK <peter.killick@informaticslab.co.uk>
2021-05-06 13:22:56 +0100

    Add test modules

---
+++ shapecutter/tests/integration/test_iris_data_gpd_geom.py
@@ -0,0 +1,51 @@
+"""
+Test shapecutter with:
+  * data source: Iris Cube
+  * geometry source: GeoPandas GeoDataFrame
+
+"""
+
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+import geopandas as gpd
+import iris
+import iris.quickplot as qplt
+import matplotlib.pyplot as plt
+
+from shapecutter.cutter import Cutter
+
+
+def plot(dataset, geometry):
+    ax = plt.axes(projection=ccrs.LambertConformal())
+    ax.coastlines()
+    qplt.pcolormesh(dataset[0])
+    geometry.plot(ax=ax, transform=ccrs.PlateCarree(),
+                  edgecolor="r", facecolor="none")
+    plt.show()
+
+
+def main(state_name):
+    # Load raster data and geometries, and define geometry selection keys.
+    a1b_cube = iris.load_cube(iris.sample_data_path("a1b_north_america.nc"))
+    us_states_name = shpreader.natural_earth(
+        category="cultural",
+        name="admin_1_states_provinces")
+    us_states = gpd.read_file(us_states_name)
+    geometry_keys = {"name": state_name}
+
+    # Construct a Cutter instance to test and extract the target geometry for plotting.
+    cutter = Cutter(a1b_cube, us_states, ignore_errors=False)
+    tgt_geometry = cutter.geometry_provider[geometry_keys]
+
+    # Test 1: cut to geometry bbox and plot for visual check.
+    bbox_cube = cutter.cut_dataset(geometry_keys)
+    plot(bbox_cube, tgt_geometry)
+
+    # Test 2: cut to geometry boundary and plot for visual check.
+    boundary_cube = cutter.cut_dataset(geometry_keys, to="boundary")
+    plot(boundary_cube, tgt_geometry)
+
+
+if __name__ == '__main__':
+    state_name = "Indiana"
+    main(state_name)
